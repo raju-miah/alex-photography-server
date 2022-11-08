4b84cce71c9c4acad0052afbc5c7b54eab7ae747
raju-miah <rajumiahpollob7@gmail.com>
2022-11-08 18:14:48 +0600

    remove wrong file

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 // wromg push
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ryrpoqq.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri)
+console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
