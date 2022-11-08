e7ab3918c6c55a31d6e6242338aef4b7b9e53731
raju-miah <rajumiahpollob7@gmail.com>
2022-11-08 18:07:39 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +12,21 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// MongoDB
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ryrpoqq.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 
 app.get('/', (req, res) => {
