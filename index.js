c8816da43915077c8cd03387c58337b41b6aa2c7
raju-miah <rajumiahpollob7@gmail.com>
2022-11-10 00:23:58 +0600

    added Put delete api

--- index.js
+++ index.js
@@ -65,11 +65,48 @@ async function run() {
             res.send(reviews);
         });
 
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const review = await reviewCollection.findOne(query);
+            res.send(review);
+        })
+
         app.post('/reviews', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result);
         });
+
+        app.put('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const update = req.body;
+            // console.log(update);
+            const option = { upsert: true };
+            const updatedReview = {
+                $set: {
+                    client: update.client,
+                    photo: update.photo,
+                    message: update.message
+                }
+
+            }
+            const result = await reviewCollection.updateOne(filter, updatedReview, option);
+            res.send(result);
+
+        });
+
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
     }
     finally {
 
